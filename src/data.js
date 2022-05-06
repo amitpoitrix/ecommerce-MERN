2e400748780c62d5ec07fb001fe888269417861f
amitpoitrix <amit.poitrix@gmail.com>
2022-05-07 00:56:15 +0530

    Creted Pure React Slider

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+        id: 1,
+        title: "SUMMER SALE",
+        img: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/16988744/2022/2/23/f4c963e6-dd44-439b-80f2-e10e1b608adb1645558630984KALINIWomenBluePrintedDress1.jpg",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "F2F2F2"
+    },
+
+    {
+        id: 2,
+        title: "AUTUMN COLLECTION",
+        img: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/2/3/2c040a86-f809-46ff-ba7c-2a2a685533691612357352168-5.jpg",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "E7EAE9"
+    },
+
+    {
+        id: 3,
+        title: "LOUNGEWEAR LOVE",
+        img: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17132170/2022/2/11/c29a70dd-b429-434a-913c-5bc4b103fa561644564800519VishudhPeach-ColouredEthnicMotifsEthnicMaxiDress1.jpg",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "F3F6F8"
+    },
+];
